89f58f37933e1cae19856632dd1b7355e0887b4e
van.tien.tu <van.tien.tu@Van-Tien-Tu.local>
2017-08-03 08:38:49 +0700

    first commit

---
+++ WorldCarthage/HelloWorld.swift
@@ -0,0 +1,15 @@
+//
+//  HelloWorld.swift
+//  WorldCarthage
+//
+//  Created by van.tien.tu on 8/3/17.
+//  Copyright © 2017 van.tien.tu. All rights reserved.
+//
+
+import UIKit
+
+public class HelloWorld: NSObject {
+    @objc public func prints() {
+        print("Hello world")
+    }
+}
---
+++ WorldCarthage/WorldCarthage.h
@@ -0,0 +1,19 @@
+//
+//  WorldCarthage.h
+//  WorldCarthage
+//
+//  Created by van.tien.tu on 8/3/17.
+//  Copyright © 2017 van.tien.tu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WorldCarthage.
+FOUNDATION_EXPORT double WorldCarthageVersionNumber;
+
+//! Project version string for WorldCarthage.
+FOUNDATION_EXPORT const unsigned char WorldCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorldCarthage/PublicHeader.h>
+
+
